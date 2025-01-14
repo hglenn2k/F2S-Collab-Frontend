05a5d73177aa96ee5c99c1f92d63392924e2fc27
Harrison Glenn <hglenn2k@gmail.com>
2025-01-14 14:02:41 -0800

    remove teambuilding component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Hero from '@/components/sections/Hero'
 import Vision from '@/components/sections/Vision'
 import ProgramHighlights from '@/components/sections/ProgramHighlights'
 import Timeline from '@/components/sections/Timeline'
-// import TeamBuilding from '@/components/sections/TeamBuilding'
 import CallToAction from '@/components/sections/CallToAction'
 
 export default function Home() {
